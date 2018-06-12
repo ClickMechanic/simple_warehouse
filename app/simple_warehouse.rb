99b206bb7b7aea3339e54cf8775133e1bc40a6b5
Felix Kenton <robotfelix@gmail.com>
2018-06-12 19:01:07 +0100

    Make help message more consistent with instructions

--- app/simple_warehouse.rb
+++ app/simple_warehouse.rb
@@ -22,10 +22,10 @@ class SimpleWarehouse
   def show_help_message
     puts 'help             Shows this help message
 init W H         (Re)Initialises the application as a W x H warehouse, with all spaces empty.
-store X Y W H P  Stores a crate of product number P and of size W x H at position X,Y.
-locate P         Show a list of positions where product number can be found.
-remove X Y       Remove the crate at positon X,Y.
-view             Show a representation of the current state of the warehouse, marking each position as filled or empty.
+store X Y W H P  Stores a crate of product code P and of size W x H at position (X,Y).
+locate P         Show a list of positions where product code P can be found.
+remove X Y       Remove the crate at position (X,Y).
+view             Output a representation of the current state of the shelves.
 exit             Exits the application.'
   end
 
