69941390a1a7a81dd124eaf5253bf73dc4c44f33
Nijeesh Joshy <nijeeshjoshy@gmail.com>
2022-08-06 15:56:59 +0530

    added x2 and y2 to create class

--- app/crate.rb
+++ app/crate.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 require './app/errors'
 
 class Crate
-
   attr_reader :x_pos, :y_pos, :width, :height, :product_code
 
+  alias x1 x_pos
+  alias y1 y_pos
   def initialize(x_pos, y_pos, width, height, product_code)
     @x_pos = x_pos.to_i
     @y_pos = y_pos.to_i
@@ -12,5 +15,13 @@ class Crate
     @product_code = product_code
 
     raise Errors::InvalidBoxDimensions if @width.zero? || @height.zero?
+  end
+
+  def x2
+    x1 + width
+  end
+
+  def y2
+    y1 + height
   end
 end
--- spec/crate_spec.rb
+++ spec/crate_spec.rb
@@ -5,14 +5,15 @@ require './app/crate'
 require './app/errors'
 
 describe Crate do
+  subject { Crate.new(1, 1, 2, 2, 'p_code') }
+
   describe '#initialize' do
     context 'sets values for attr readers' do
-      subject { Crate.new(0, 0, 1, 1, 'p_code') }
 
-      it { expect(subject.x_pos).to eq(0) }
-      it { expect(subject.y_pos).to eq(0) }
-      it { expect(subject.width).to eq(1) }
-      it { expect(subject.height).to eq(1) }
+      it { expect(subject.x_pos).to eq(1) }
+      it { expect(subject.y_pos).to eq(1) }
+      it { expect(subject.width).to eq(2) }
+      it { expect(subject.height).to eq(2) }
       it { expect(subject.product_code).to eq('p_code') }
 
       context 'when coordinates are passed as string' do
@@ -33,6 +34,25 @@ describe Crate do
           described_class.new(0, 0, 0, 0, 'p_code')
         end.to raise_error(Errors::InvalidBoxDimensions)
       end
+    end
+  end
+
+  describe 'alias methods' do
+    it 'same return value for both methods' do
+      expect(subject.x_pos).to eq(subject.x1)
+      expect(subject.y_pos).to eq(subject.y1)
+    end
+  end
+
+  describe '#x2' do
+    it 'is sum of x1 + width' do
+      expect(subject.x2).to eq(3)
+    end
+  end
+
+  describe '#y2' do
+    it 'is sum of y1 + height' do
+      expect(subject.y2).to eq(3)
     end
   end
 end
