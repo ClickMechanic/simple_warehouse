e2be9cce59a4bd76d39c5e585d99f09a7a8df508
Nijeesh Joshy <nijeeshjoshy@gmail.com>
2022-08-06 19:20:52 +0530

    moved the logging functionality to separate logger class

--- app/simple_warehouse.rb
+++ app/simple_warehouse.rb
@@ -1,12 +1,15 @@
 require './app/ware_house_std_in_adapter'
+require './app/warehouse_logger'
 
 class SimpleWarehouse
 
   def run
     @live = true
     @adapter = WareHouseStdInAdapter.new
+    @logger = WarehouseLogger.new
 
-    puts 'Type `help` for instructions on usage'
+    @logger.green 'Type `help` for instructions on usage'
+
     while @live
       print '> '
       input = gets.chomp.split(/\s+/)
@@ -16,35 +19,21 @@ class SimpleWarehouse
       when 'help'
         show_help_message
       when 'init', 'store', 'locate', 'remove', 'view'
-        @adapter.public_send(command, *arguments)
+        @logger.handle_command_response(command) do
+          @adapter.public_send(command, *arguments)
+        end
       when 'exit'
         exit
       else
-        show_unrecognized_message
+        @logger.show_unrecognized_message
       end
     end
   end
 
   private
 
-  def show_help_message
-    puts <<~HELP
-      help             Shows this help message
-      init W H         (Re)Initialises the application as an empty W x H warehouse.
-      store X Y W H P  Stores a crate of product code P and of size W x H at position (X,Y).
-      locate P         Show a list of all locations occupied by product code P.
-      remove X Y       Remove the entire crate occupying the location (X,Y).
-      view             Output a visual representation of the current state of the grid.
-      exit             Exits the application.
-    HELP
-  end
-
-  def show_unrecognized_message
-    puts 'Command not found. Type `help` for instructions on usage'
-  end
-
   def exit
-    puts 'Thank you for using simple_warehouse!'
+    @logger.green 'Thank you for using simple_warehouse!'
     @live = false
   end
 
---
+++ app/warehouse_logger.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+require 'rainbow'
+require './app/errors'
+
+class WarehouseLogger
+  COMMANDS = {
+    'help' => 'Shows this help message',
+    'init' => 'W H         (Re)Initialises the application as an empty W x H warehouse.',
+    'store' => 'X Y W H P  Stores a crate of product code P and of size W x H at position (X,Y).',
+    'locate' => 'P         Show a list of all locations occupied by product code P.',
+    'remove' => 'X Y       Remove the entire crate occupying the location (X,Y).',
+    'view' => 'Output a visual representation of the current state of the grid.',
+    'exit' => 'Exits the application.'
+  }.freeze
+
+  def info(message)
+    puts Rainbow(message).blue
+  end
+
+  def error(message)
+    puts Rainbow(message).red
+  end
+
+  def warn(message)
+    puts Rainbow(message).yellow
+  end
+
+  def green(message)
+    puts Rainbow(message).green
+  end
+
+  def show_help_message_for(command)
+    info command.ljust(10) + COMMANDS[command]
+  end
+
+  def show_help_message
+    COMMANDS.each_key { |command| show_help_message_for(command) }
+  end
+
+  def handle_command_response(command)
+    response = yield
+    send(command_response_name(command), response)
+  rescue ArgumentError => e
+    error "Invalid arguments send to #{command}\n"
+    show_help_message_for(command)
+  rescue Errors::WareHouseNotInitializedError => e
+    show_uninitialized_message
+  rescue Errors::WarehouseError => e
+    error e.message
+  end
+
+  def show_unrecognized_message
+    error 'Command not found. Type `help` for instructions on usage'
+  end
+
+  def show_uninitialized_message
+    warn 'Warehouse not initialized. Please initialize the Warehouse to continue'
+    show_help_message_for('init')
+  end
+
+  def show_locate_command_response(boxes)
+    return warn 'No boxes found' if boxes.empty?
+
+    boxes.each do |box|
+      info "Box named #{box.name} found at #{box.x_pos}, #{box.y_pos}"
+    end
+  end
+
+  def show_remove_command_response(_)
+    green 'Box removed'
+  end
+
+  def show_store_command_response(_)
+    green 'Box stored'
+  end
+
+  def show_init_command_response(_)
+    green 'Warehouse initialized'
+  end
+
+  def show_view_command_response(_); end
+
+  private
+
+  def command_response_name(command)
+    "show_#{command}_command_response"
+  end
+end
